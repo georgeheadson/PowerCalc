3a9fdd5067b66a48373c326448b8a0fc5070633b
georgeheadson <georgeheadson@mail.ru>
2024-09-05 11:33:17 +0300

    delete no usages

--- src/main/java/ve/powercalc/controllers/DefaultController.java
+++ src/main/java/ve/powercalc/controllers/DefaultController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ve.powercalc.PowerCount;
-import ve.powercalc.entity.Dates;
 import ve.powercalc.entity.Group;
 import ve.powercalc.services.GroupCRUDService;
 import java.io.File;
